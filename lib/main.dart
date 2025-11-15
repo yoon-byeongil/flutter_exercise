641e79ccf40379a8477fff4fc767fe26bbb85da7
yoon-byeongil <justbrand101@gmail.com>
2025-11-16 00:08:55 +0900

    exercise 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,15 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello flutter!')),
+        body: Center(child: Text('Hello World!')),
+      ),
+    );
   }
 }
